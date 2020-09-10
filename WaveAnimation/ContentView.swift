7541972cc12aa03025f72b7e8db208764514a7db
Henry Hudson <henryhudson1431@yahoo.com>
2020-09-11 00:13:46 +0100

    Initial Commit

---
+++ WaveAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaveAnimation
+//
+//  Created by Henry Hudson on 11/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaveAnimation/WaveAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveAnimationApp.swift
+//  WaveAnimation
+//
+//  Created by Henry Hudson on 11/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct WaveAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
